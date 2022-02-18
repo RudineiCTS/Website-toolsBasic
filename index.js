800154d5dd955cd896a1cb60f2b249a033743098
Rudinei Augusto Costa Ferreira <rudinei.ferreira@linx.com.br>
2022-02-18 13:27:09 -0300

    animate page content main

---
+++ index.js
@@ -0,0 +1,46 @@
+// img-presentation
+// content-text-presentation
+// subititle-2
+// lodash
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+target.forEach((element)=>{
+    // console.log(element.offsetTop)
+    if(windowTop > element.offsetTop){
+        element.classList.add(animationClass);
+    }else{
+        element.classList.remove(animationClass);
+    }
+    
+} )
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', debounce(() => {
+        animeScroll();
+        console.log('ola');
+    }, 200))
+}

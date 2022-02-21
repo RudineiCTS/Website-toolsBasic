b519e025ab4cb4407c4b0b69bd9b37f03a04b43b
Rudinei Augusto Costa Ferreira <rudinei.ferreira@linx.com.br>
2022-02-21 17:59:44 -0300

    ajuste in section

--- script-img.js
+++ script-img.js
@@ -2,11 +2,13 @@ const containerImg = document.getElementById('img-alvo')
 const buttonChangeEnemy = document.getElementsByName('change-enemy');
 
 let indexImgs =0;
-const vectorImg =['ibis.png', 'Rudjek.png']
+const vectorImg =['./enemys/ibis.png', './enemys/Rudjek.png']
 
 function handleChangeEnemy(){
   let index= containerImg.getAttribute('indexImg');
   index++;
+  console.log(index);
   if(index !== indexImgs)
   containerImg.setAttribute('src',vectorImg[index])
+  console.log(vectorImg[index]);
 }
